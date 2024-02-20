dd510df2c6b8490670a94c26006218df6a365e9c
Andres Revollo <andres.revollo@usb.edu.bo>
2024-02-19 22:14:13 -0400

    Saludo tomando encuenta la hora del dia

--- src/presenter.js
+++ src/presenter.js
@@ -1,21 +1,17 @@
 import saludar from "./saludar";
+import saludarHora from "./saludarHora";
 
 const nombre_input = document.querySelector("#nombre");
 const genero_select = document.querySelector("#genero");
 const edad_input = document.querySelector("#edad");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_input.value;
   const genero = genero_select.value;
-  const edad = edad_input.value;
+  const edad = parseInt(edad_input.value);
 
-  div.innerHTML = "<p> Hola " + saludar(nombre, genero, edad) + ".</p>";
+  div.innerHTML = "<p> " + saludarHora() + saludar(nombre, genero, edad) + ".</p>";
 });
-
-function saludar(nombre) {
-  return nombre;
-}
