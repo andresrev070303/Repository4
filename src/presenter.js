9c8566cf38bff59efcdb0b00416020da702bd0f8
Andres Revollo <andres.revollo@usb.edu.bo>
2024-02-19 21:43:47 -0400

    Saludo tomando enc uenta el genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,20 +1,20 @@
 import saludar from "./saludar";
 
-
 const nombre_input = document.querySelector("#nombre");
+const genero_select = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  
   const nombre = nombre_input.value;
+  const genero = genero_select.value;
 
-  
-div.innerHTML = "<p> Hola " + saludar(nombre) + ".</p>";
+  div.innerHTML = "<p> Hola " + saludar(nombre, genero) + ".</p>";
 });
 
-function saludar(nombre) {
+function saludar(nombre) 
+{
   return nombre;
 }
